b98c1f6449610585fe2c2e7076b0270517fb8f77
supunyasantha <supunyasantha.official@gmail.com>
2025-10-21 11:13:35 +0530

    remove footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,14 +15,6 @@ const Footer = () => {
             </a>
           </div>
 
-          <div className="footer-links">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
-          </div>
-
           <div className="footer-social">
             <a href="#" aria-label="Twitter">
               <i className="fab fa-twitter"></i>
